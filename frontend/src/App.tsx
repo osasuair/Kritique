aec20fc511484e8baa84d1ee8c107da616d8caad
victorr <kolawoledamilola06@gmail.com>
2025-01-18 13:31:38 -0500

    frontend init and main page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Hero from "./components/Hero";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Hero />;
+}
+
+export default App;
