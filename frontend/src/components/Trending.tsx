84583bc7c2e89be9d989a6834e830eb57a2c5aa5
victorr <kolawoledamilola06@gmail.com>
2025-01-19 02:48:37 -0500

    trending tab and add site

--- frontend/src/components/Trending.tsx
+++ frontend/src/components/Trending.tsx
@@ -1,12 +1,13 @@
-const Trending = () => {
+const Trending = ({ trends }: { trends: string[] }) => {
   return (
     <div className="m-auto w-auto h-auto text-red-400 leading-7 rounded-[20px] bg-gray-700 p-5">
       <h2 className="text-[20px] border-b">Trending💫</h2>
       <ul>
-        <li>www.spotify.com</li>
-        <li>www.applemusic.com</li>
-
-        <li>www.betting.com</li>
+        {trends.length > 0 ? (
+          trends.map((website, index) => <li key={index}>{website}</li>)
+        ) : (
+          <li className="p-4 text-gray-500">No trending websites available.</li>
+        )}
       </ul>
     </div>
   );
